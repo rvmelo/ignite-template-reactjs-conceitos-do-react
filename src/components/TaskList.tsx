576ca1a4e3f07ff778d867973cca98cf8367e41c
rvmelo <robertomelo7@hotmail.com>
2021-04-30 11:10:19 -0300

    challenge finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,38 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if(!newTaskTitle) return;
+
+    setTasks(prev => [...prev, {id: Math.floor(Math.random() * 101), 
+      title: newTaskTitle, isComplete: false }]);
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const auxTasks = tasks.map(task => {
+
+      if(task.id === id) task.isComplete = !task.isComplete;
+
+      return task;
+
+    });
+
+
+    setTasks([...auxTasks]);
+
+
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const auxTasks = tasks.filter(task => task.id !== id);
+
+    setTasks([...auxTasks]);
+
   }
 
   return (
